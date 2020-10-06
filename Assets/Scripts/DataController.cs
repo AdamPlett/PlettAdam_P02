46638666d8511fd0d6a24c9343836e65bec79c01
Adam <Adamqplett@gmail.com>
2020-10-05 20:47:13 -0500

    FPS UI Setup
    The UI elements of the FPS game have been set up

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataController : MonoBehaviour
+{
+    [SerializeField] Text _highScoreTextView;
+    string HighScore = "HighScore";
+    void Update()
+    {
+        //load high score display
+        int highScore = PlayerPrefs.GetInt(HighScore);
+        _highScoreTextView.text = highScore.ToString();
+    }
+    public void ResetData()
+    {
+        PlayerPrefs.SetInt(HighScore, 0);
+    }
+}
---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level01Controller : MonoBehaviour
+{
+    [SerializeField] Text _currentScoreTextView;
+    [SerializeField] GameObject _MenuPanel;
+
+    int _currentScore;
+    string HighScore = "HighScore";
+    void Update()
+    {
+        //brings up pop up menu
+        if ((Input.GetKeyDown(KeyCode.Escape)) && (_MenuPanel.activeSelf == false) )
+        {
+            PopUpMenu();
+        }
+        else if ((Input.GetKeyDown(KeyCode.Escape)) && (_MenuPanel.activeSelf == true))
+        {
+            Resume();
+        }
+        //increase score
+        //TODO replace with real implementation later
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+    }
+    public void IncreaseScore(int ScoreIncrease)
+    {
+        //increase score
+        _currentScore += ScoreIncrease;
+        //update socre display, so we can see the new scoer
+        _currentScoreTextView.text = "Score: " + _currentScore.ToString();
+    }
+    public void ExitLevel()
+    {
+        //compare score to highscore
+        int highScore = PlayerPrefs.GetInt(HighScore);
+        if (_currentScore > highScore)
+        {
+            //save as new highscore
+            PlayerPrefs.SetInt(HighScore, _currentScore);
+            Debug.Log("New high score: " + _currentScore);
+        }
+        //load main menu scene
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void PopUpMenu()
+    {
+        //makes pop up menu visible
+        _MenuPanel.SetActive(true);
+        //TODO unlock and unhide cursor
+    }
+    public void Resume()
+    {
+        //makes pop up menu disappear
+        _MenuPanel.SetActive(false);
+        //TODO lock and hide cursor
+    }
+}
