b8f446c72e320fbf7835e8f904b7e10fa312f049
Adam <Adamqplett@gmail.com>
2020-10-11 01:46:30 -0500

    added keyboard based movement

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    //mouse sensitivty
     public float mouseSensitivity = 100f;
+    //player body
     public Transform playerBody;
+    //vertical rotation variable
     float xRotation = 0f;
     // Start is called before the first frame update
     void Start()
     {
+        //locks and hides cursor upon start of level
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -17,13 +21,15 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //gets mousemovement according to x and y axis
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         xRotation -= mouseY;
-
+        //clamps camera rotation so you can't look past an unrealistic angle
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
+        //rotates camera horizontally
         playerBody.Rotate(Vector3.up * mouseX);
+        //rotates camera vertically
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Player Controller
+    public CharacterController controller;
+    //ground check
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    //jump height
+    public float jumpHeight = 3f;
+    //Speed of player
+    public float speed = 12f;
+    //gravitational velocity
+    Vector3 gVelocity;
+    //gravity
+    public float gravity = -9.81f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && gVelocity.y < 0)
+        {
+            gVelocity.y = -2f;
+        }
+        //gets movement from the keyboard
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        //applies movement to vector 3
+        Vector3 move = transform.right * x + transform.forward * z;
+        //moves the player character according to the vector 3
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded )
+        {
+            gVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        gVelocity.y += gravity * Time.deltaTime;
+
+        controller.Move(gVelocity * Time.deltaTime);
+    }
+}
