4315c989429c270539786cbd1014451da5b1e812
Adam <Adamqplett@gmail.com>
2020-10-26 20:12:38 -0500

    Added Randomization for Position to Enemy Spawner
    Enemies will now spawn anywhere on the map

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,25 +7,35 @@ public class EnemySpawner : MonoBehaviour
     public GameObject Enemy;
     public float SpawnTime = 7.5f;
     private float SpawnTimer;
+    private float randX, randZ;
+    private Vector3 SpawnPoint;
     // Start is called before the first frame update
     void Start()
     {
         SpawnTimer = SpawnTime;
+        SpawnPoint = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(transform.position.x + " " + transform.position.y + " " + transform.position.z);
         SpawnTimer -= Time.deltaTime;
         if (SpawnTimer<=0)
         {
-            Instantiate(Enemy, transform.position, Quaternion.identity);
+            randX = Random.Range(-43.2f,37.8f);
+            randZ = Random.Range(-35.98f, 47.55f);
+            SpawnPoint = SpawnPoint + new Vector3(randX,0,randZ);
+            Instantiate(Enemy, SpawnPoint, Quaternion.identity);
+            Debug.Log(SpawnPoint.x + " " + SpawnPoint.y + " " + SpawnPoint.z);
             ResetTimer();
         }
     }
     private void ResetTimer()
     {
         SpawnTimer = SpawnTime;
+        SpawnPoint = transform.position;
+        //Debug.Log(SpawnPoint.x + " " + SpawnPoint.y + " " + SpawnPoint.z);
         if (SpawnTime>1.5) SpawnTime=SpawnTimer-.5f;
     }
 }
